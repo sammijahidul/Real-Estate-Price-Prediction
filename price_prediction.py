df06ab8b28bca8ecfcab4abba04e95f81ec43a45
sammijahidul <jahidul.islam03@northsouth.edu>
2021-06-01 02:07:59 +0600

    just set up the project

---
+++ price_prediction.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.rcParams["figure.figsize"] = (20,10)
+
+data_set = pd.read_csv("Bengaluru_House_Data.csv")
+data_set.head()
+
